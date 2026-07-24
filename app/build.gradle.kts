82785a448776e718b0a677d6411e41bcd7a06c56
neidersalgado <hsneider.salgado@hotmail.com>
2026-07-24 10:15:37 -0500

    fix: add :feature:deck dependency to :app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":feature:deck"))
     implementation(platform("androidx.compose:compose-bom:2024.10.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
